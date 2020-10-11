69154ad1f2f87e10439c1e232e140929338eacd4
FiqSky <fiqihfm05@gmail.com>
2020-10-11 10:52:54 +0700

    all fixed and add SplashScreenActivity.kt

---
+++ app/src/main/java/com/fiqsky/githubuserapp/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.fiqsky.githubuserapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.fiqsky.githubuserapp.ui.activity.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    private val timeOut : Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, timeOut)
+    }
+}
